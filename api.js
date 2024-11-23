c2583ca8e3edf44bfae3b81cebd8b90c1c54b8db
suchitkumarchennuri <143919454+suchitkumarchennuri@users.noreply.github.com>
2024-11-23 23:16:09 +0000

    created products and orders endpoints.

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const Products = require('./products')
+const Orders = require('./orders')
 const autoCatch = require('./lib/auto-catch')
 
 /**
@@ -49,9 +50,9 @@ async function getProduct(req, res, next) {
  * @param {object} req 
  * @param {object} res 
  */
-async function createProduct(req, res) {
-  console.log('request body:', req.body)
-  res.json(req.body)
+async function createProduct (req, res, next) {
+  const product = await Products.create(req.body)
+  res.json(product)
 }
 
 /**
@@ -60,19 +61,57 @@ async function createProduct(req, res) {
  * @param {object} res
  * @param {function} next
  */
-async function editProduct(req, res, next) {
-  console.log(req.body)
-  res.json(req.body)
+async function editProduct (req, res, next) {
+  const change = req.body
+  const product = await Products.edit(req.params.id, change)
+  res.json(product)
 }
-
 /**
  * Delete a product
  * @param {*} req 
  * @param {*} res 
  * @param {*} next 
  */
-async function deleteProduct(req, res, next) {
-  res.json({ success: true })
+async function deleteProduct (req, res, next) {
+  const response = await Products.destroy(req.params.id)
+  res.json(response)
+}
+
+async function createOrder (req, res, next) {
+  const order = await Orders.create(req.body)
+  res.json(orders)
+}
+
+async function listOrders (req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query
+
+  const orders = await Orders.list({ 
+    offset: Number(offset), 
+    limit: Number(limit),
+    productId, 
+    status 
+  })
+
+  res.json(orders)
+}
+async function editOrder(req, res, next) {
+  try {
+    const { id } = req.params;
+    const changes = req.body;
+    const updatedOrder = await Orders.edit(id, changes);
+    res.json(updatedOrder);
+  } catch (err) {
+    next(err);
+  }
+}
+async function deleteOrder(req, res, next) {
+  try {
+    const { id } = req.params;
+    await Orders.destroy(id); // Call the destroy method
+    res.status(204).send(); // Send a 204 No Content status
+  } catch (err) {
+    next(err); // Pass the error to the error-handling middleware
+  }
 }
 
 module.exports = autoCatch({
@@ -81,5 +120,9 @@ module.exports = autoCatch({
   getProduct,
   createProduct,
   editProduct,
-  deleteProduct
+  deleteProduct,
+  createOrder,
+  listOrders,
+  editOrder,
+  deleteOrder
 });
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+ const express = require('express')
 const api = require('./api')
 const middleware = require('./middleware')
 const bodyParser = require('body-parser')
@@ -19,6 +19,10 @@ app.get('/products/:id', api.getProduct)
 app.put('/products/:id', api.editProduct)
 app.delete('/products/:id', api.deleteProduct)
 app.post('/products', api.createProduct)
+app.get('/orders', api.listOrders)
+app.get('/orders/', api.createOrder)
+app.put('/orders/:id', api.editOrder)
+app.delete('/orders/:id',api.deleteProduct)
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
